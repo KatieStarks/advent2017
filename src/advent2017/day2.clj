0129c21978cbe9ff07f3f149158a0217a80038aa
KatieStarks <katie.ballinger@gmail.com>
2017-12-02 16:20:25 -0800

    day2 part2

--- src/advent2017/day2.clj
+++ src/advent2017/day2.clj
@@ -19,9 +19,32 @@
   (map #(map read-string (str/split % #"\t")) (str/split-lines input)))
 
 (defn checksum [row]
-  (let [mn (apply min row)
-        mx (apply max row)]
-        (- mx mn)))
+  (- (apply max row) (apply min row)))
 
-(defn -main [input]
-  (map checksum (make-table input)))
+(defn -main1 [input]
+  (reduce + (map checksum (make-table input))))
+
+;; PART TWO
+;
+;the goal is to find the only two numbers in each row where one evenly divides the other - that is, where the result of the division operation is a whole number. They would like you to find those numbers on each line, divide them, and add up each line's result.
+;
+;For example, given the following spreadsheet:
+;
+;5 9 2 8
+;9 4 7 3
+;3 8 6 5
+;In the first row, the only two numbers that evenly divide are 8 and 2; the result of this division is 4.
+;In the second row, the two numbers are 9 and 3; the result is 3.
+;In the third row, the result is 2.
+;In this example, the sum of the results would be 4 + 3 + 2 = 9.
+
+(defn check-divisible [row]
+  (reduce +
+    (for [x row y row
+          :when (not= x y)]
+      (if (zero? (mod x y))
+        (/ x y)
+        0))))
+
+(defn -main2 [input]
+  (reduce + (map check-divisible (make-table input))))
