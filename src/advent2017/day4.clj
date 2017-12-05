ea05518d8351f14fbedd5c575483292cf14fe0a8
KatieStarks <katie.ballinger@gmail.com>
2017-12-05 15:28:21 -0800

    day4 part two

--- src/advent2017/day4.clj
+++ src/advent2017/day4.clj
@@ -17,8 +17,8 @@
 ; How many passphrases are valid?
 
 (def input (slurp "resources/day4input.txt"))
-(def test-pass "aa bb cc dd aaa")
-(def test-fail "aa bb cc dd aa")
+(def test-pass "a ab abc abd abf abj")
+(def test-fail "abcde xyz ecdab abcde")
 
 (defn separate-passes [input]
   (map #(str/split % #" ") (str/split-lines input)))
@@ -29,5 +29,30 @@
     1
     0))
 
-(defn -main [input]
+(defn -main1 [input]
   (reduce + (map find-diff (separate-passes input))))
+
+;For added security, yet another system policy has been put in place. Now, a
+; valid passphrase must contain no two words that are anagrams of each other -
+; that is, a passphrase is invalid if any word's letters can be rearranged to
+; form any other word in the passphrase.
+;
+;For example:
+;
+;abcde fghij is a valid passphrase.
+;abcde xyz ecdab is not valid - the letters from the third word can be rearranged to form the first word.
+;a ab abc abd abf abj is a valid passphrase, because all letters need to be used when forming another word.
+;iiii oiii ooii oooi oooo is valid.
+;oiii ioii iioi iiio is not valid - any of these words can be rearranged to form any other word.
+
+;Under this new system policy, how many passphrases are valid?
+
+(defn anagrams? [pass]
+  (let [sorted (map sort pass)]
+    (if (= (count (distinct sorted))
+           (count sorted))
+      1
+      0)))
+
+(defn -main2 [input]
+  (reduce + (map anagrams? (separate-passes input))))
